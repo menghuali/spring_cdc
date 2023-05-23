2c71c2be7ba849a22142261ae124dea065ff8227
menghuali <menghua.li.1980@gmail.com>
2023-05-23 16:44:38 -0400

    init

---
+++ src/main/java/com/example/spring/cdc/SpringCdcApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.cdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/cdc/SpringCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
